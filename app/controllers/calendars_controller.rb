99a7c35c2d9b7d5aa269dc3cdc50e64bba1460f6
mitalin1210 <141559728+mitalin1210@users.noreply.github.com>
2023-08-29 18:46:08 +0900

    Issue5

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,9 @@
 class CalendarsController < ApplicationController
+  def create
+    binding.pry
+    Plan.create(plan_params)
+    redirect_to action: :index
+    end
 
   # １週間のカレンダーと予定が表示されるページ
   def index
@@ -15,7 +20,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
